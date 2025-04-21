cdf698662b46953cf2acfa8fae8ff10f79abb33a
Karthik-beta <karthikpar@protonmail.com>
2025-04-21 21:37:31 +0530

    Update Services.tsx

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -34,14 +34,14 @@ export const Services = () => {
     <section className="container py-24 sm:py-32">
       <div className="grid lg:grid-cols-[1fr,1fr] gap-8 place-items-center">
         <div>
-          <h2 className="text-3xl md:text-4xl font-bold">
+          <h2 className="text-3xl md:text-4xl font-bold text-center">
             <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
             Our{" "}
             </span>
             Services
           </h2>
 
-          <p className="text-muted-foreground text-xl mt-4 mb-8 ">
+          <p className="text-muted-foreground text-xl mt-4 mb-8 text-center">
             We offer a comprehensive suite of technology services designed to empower your enterprise, from strategic implementation to robust infrastructure and ongoing support.
           </p>
 
